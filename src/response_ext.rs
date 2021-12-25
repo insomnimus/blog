b09c6b4e968f75ea6b32c1f680a877f00c497ea2
Taylan Gökkaya <insomnimus.dev@gmail.com>
2021-12-25 23:39:36 +0300

    refactor: tidied up response_ext

--- src/response_ext.rs
+++ src/response_ext.rs
@@ -3,42 +3,27 @@ use axum::{
 	response::Html,
 };
 
-pub trait ResultExt<T: Sized, E> {
+pub trait ResultExt<T, E>: Sized {
 	fn or_code_with(self, code: StatusCode, msg: impl AsRef<str>) -> Result<T, StatusCode>;
 	fn html(self) -> Result<Html<T>, E>;
 
-	fn or_code(self, code: StatusCode) -> Result<T, StatusCode>
-	where
-		Self: Sized,
-	{
+	fn or_code(self, code: StatusCode) -> Result<T, StatusCode> {
 		self.or_code_with(code, "")
 	}
 
-	fn or_404_with(self, msg: impl AsRef<str>) -> Result<T, StatusCode>
-	where
-		Self: Sized,
-	{
+	fn or_404_with(self, msg: impl AsRef<str>) -> Result<T, StatusCode> {
 		self.or_code_with(StatusCode::NOT_FOUND, msg)
 	}
 
-	fn or_404(self) -> Result<T, StatusCode>
-	where
-		Self: Sized,
-	{
+	fn or_404(self) -> Result<T, StatusCode> {
 		self.or_code(StatusCode::NOT_FOUND)
 	}
 
-	fn or_503_with(self, msg: impl AsRef<str>) -> Result<T, StatusCode>
-	where
-		Self: Sized,
-	{
+	fn or_503_with(self, msg: impl AsRef<str>) -> Result<T, StatusCode> {
 		self.or_code_with(StatusCode::INTERNAL_SERVER_ERROR, msg)
 	}
 
-	fn or_503(self) -> Result<T, StatusCode>
-	where
-		Self: Sized,
-	{
+	fn or_503(self) -> Result<T, StatusCode> {
 		self.or_code(StatusCode::INTERNAL_SERVER_ERROR)
 	}
 }
