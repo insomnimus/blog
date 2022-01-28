6d0d0bc52da26e581c5e77273091ed81c4976a74
Taylan Gökkaya <insomnimus@protonmail.com>
2022-01-29 00:21:57 +0300

    refactor(js): special cased singular intervals (1 day ago -> yesterday)

--- static/localize_dates.js
+++ static/localize_dates.js
@@ -7,21 +7,38 @@ for (const date of dates) {
 }
 
 function format_since(date) {
-	const secs = (new Date() - date) / 1000;
-	const intervals = [
-		["second", 1, 60],
-		["minute", 60, 3600],
-		["hour", 3600, 86400],
-		["day", 86400, 1209600],
-		["week", 604800, 3628800],
-		["month", 2592000, 46656000],
+	const month_names = [
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December",
 	];
 
-	for (const [name, in_sec, up_to] of intervals) {
+	const intervals = [
+		["second", 1, 60, "just now"],
+		["minute", 60, 360, "a minute ago"],
+		["hour", 3600, 86400, "an hour ago"],
+		["day", 86400, 1209600, "yesterday"],
+		["week", 604800, 3628800, "last week"],
+		["month", 2592000, 31536000, "last month"],
+	];
+
+	const secs = (new Date() - date) / 1000;
+
+	for (const [name, in_sec, up_to, special_case] of intervals) {
 		if (secs < up_to) {
 			const n = Math.floor(secs / in_sec);
 			if (n == 1) {
-				return `${n} ${name} ago`;
+				// return `1 ${name} ago`;
+				return special_case;
 			} else {
 				return `${n} ${name}s ago`;
 			}
@@ -30,7 +47,8 @@ function format_since(date) {
 
 	const n = Math.floor(secs / 31536000);
 	if (n == 1) {
-		return `${n} year ago`;
+		const month = month_names[date.getMonth()];
+		return `last year on ${month}`;
 	} else {
 		return `${n} years ago`;
 	}
